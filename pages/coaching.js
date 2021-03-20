cc93b21cd25e4c282915e8e4be3e6dbba065a853
Daniel Roberts <danielrobertsdesign@gmail.com>
2021-03-20 18:57:39 -0400

    Add Contact page

--- pages/coaching.js
+++ pages/coaching.js
@@ -1,4 +1,5 @@
 import Layout from '../components/layout'
+import Contact from '../components/contact'
 import HeroBlock from '../components/HeroBlock' 
 
 function Coaching() {
@@ -6,6 +7,7 @@ function Coaching() {
     <Layout>
       <HeroBlock>With 1 on 1 coaching, I can craft a personalized curriculum for you to learn how to program, with guidance and help troubleshooting.</HeroBlock>
       <HeroBlock>My hourly rate is $75/hr.</HeroBlock>
+      <Contact />
     </Layout>
   )
 }
